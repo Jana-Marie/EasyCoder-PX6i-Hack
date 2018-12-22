1b9a5a0f215a50b3531e1445ab58a5b31a31753e
Jan--Henrik <janhenrik@janhenrik.org>
2018-12-22 23:58:30 +0100

    now interupt based

--- Labeldrucker/Labeldrucker.ino
+++ Labeldrucker/Labeldrucker.ino
@@ -1,41 +1,53 @@
 #define AIN     A0
 #define OUT     9
 #define FILT    0.45F
-#define TRIG    0.02F
-#define ONTIME  80
-#define DELAY   100
+#define TRIG    0.01F
+#define ONTIME  20
+#define DELAY   10
+#define STEP    2
 
 struct var {
   float ain;
   float avg;
   float delt;
-  uint16_t tim;
+  volatile uint16_t cnt;
   bool state;
 } v;
+
+void cnt_isr() {
+  v.cnt++;
+}
 
 void setup() {
   pinMode(AIN, INPUT);
   pinMode(OUT, OUTPUT);
-  digitalWrite(OUT,HIGH);
+  pinMode(STEP, INPUT);
+  digitalWrite(OUT, HIGH);
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
   v.state = 1;
   v.ain = analogRead(AIN);
   v.avg = analogRead(AIN);
+
+  attachInterrupt(digitalPinToInterrupt(STEP), cnt_isr, RISING);
 }
 
 void loop() {
   if (analogRead(AIN) > 200) {
     v.ain = analogRead(AIN);
     v.avg = FILT * v.avg + ((1.0F - FILT) * v.ain);
-    
-    Serial.print(v.ain);
-    Serial.print(" ");
-    Serial.println(v.avg);
+
+    //Serial.print(v.ain);
+    //Serial.print(" ");
+    Serial.println(v.cnt);
 
     v.delt = v.ain - v.avg;
     if ( v.delt > TRIG) {
-      delay(DELAY);
+      v.cnt = 0;
+      while (true) {
+        if (v.cnt > DELAY) break;
+      }
+      Serial.println(v.cnt);
       WriteIO(OUT, 0);
       delay(ONTIME);
       WriteIO(OUT, 1);
@@ -44,7 +56,7 @@ void loop() {
     v.ain = analogRead(AIN);
     v.avg = analogRead(AIN);
   }
-//  Serial.println(v.avg);
+  //Serial.println(v.cnt);
 }
 
 void WriteIO(int io, bool state) {
